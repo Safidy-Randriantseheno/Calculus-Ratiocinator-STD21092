b4c654e4621e6fa9823eb70555b5ed7618dbc853
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-07 15:52:09 +0300

    feat: test si l'affirmation Lou est beau vraiment est Vraie .

--- src/test/java/com/examen/CalculusRatiocinator/Affirmation/CalculusRatiocinatorTest.java
+++ src/test/java/com/examen/CalculusRatiocinator/Affirmation/CalculusRatiocinatorTest.java
@@ -1,4 +1,16 @@
 package com.examen.CalculusRatiocinator.Affirmation;
 
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CalculusRatiocinatorTest {
+    @Test
+    public void testLouEstBeauEstVraie() {
+        Affirmation affirmationLouEstBeau = new Vraie();
+        assertEquals("vrai", AffirmationEvaluator.evaluerAffirmation(affirmationLouEstBeau));
+    }
+
 }
