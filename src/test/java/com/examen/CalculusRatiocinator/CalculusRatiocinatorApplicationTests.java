55e2e8c951c167b4cd8d158f024b88025be7018c
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-07 13:24:04 +0300

    first commit

---
+++ src/test/java/com/examen/CalculusRatiocinator/CalculusRatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.CalculusRatiocinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculusRatiocinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
