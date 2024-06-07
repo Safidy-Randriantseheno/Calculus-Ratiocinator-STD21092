c42b4573f946303c96c206efa84c33d7479d67db
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-07 16:27:04 +0300

    chore: remove something .

--- src/test/java/com/examen/CalculusRatiocinator/Affirmation/CalculusRatiocinatorTest.java
+++ src/test/java/com/examen/CalculusRatiocinator/Affirmation/CalculusRatiocinatorTest.java
@@ -1,10 +1,8 @@
 package com.examen.CalculusRatiocinator.Affirmation;
 
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculusRatiocinatorTest {
     @Test
